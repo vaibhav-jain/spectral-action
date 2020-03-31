bc18b204fed48c5dbcff71e5637f19566a354c0c
vaibhav-jain <vaibhav10jain@gmail.com>
2020-03-31 20:44:13 +0530

    Update action.yaml

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo "======================"
 echo "= Linting OAS file   ="
 echo "======================"
 
-cd /github/workspace/ && spectral lint "${INPUT_ARGS[@]}"
+cd /github/workspace/ && spectral lint "$INPUT_FILE_PATH"
