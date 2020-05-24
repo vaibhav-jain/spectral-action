8fc3b765fb07401de0c10114dbf45e3a4c433e98
Marin Petrunić <marin.petrunic@gmail.com>
2020-05-24 23:09:06 +0200

    remove extra text

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,4 @@
 echo "==============================="
 echo "= Running swagger-cli command ="
 echo "==============================="
-echo "Executing command $*"
-echo "Test"
 cd /github/workspace/ && swagger-cli $*
